442994d0689bdfcb9994694f54ab02954e584221
Tony Ivanov <telamohn@gmail.com>
2012-06-28 00:39:44 +0200

    Committing wierd changes

--- app/src/start.coffee
+++ app/src/start.coffee
@@ -1,5 +1,5 @@
 window.StatusController = ($scope) ->
   $scope.today = () ->
     Date()
-
+  $scope.week=23
   this
--- watch.sh
+++ watch.sh
@@ -1,1 +1,1 @@
-node_modules/.bin/watcher -o app/gen/ app/
+node_modules/.bin/watcher -o app/gen/ app/src/ app/style/
